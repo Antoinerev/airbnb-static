a0c5725402add72ea050cc854883e5f490d4c637
Antoine Reveau <antoinerev@gmail.com>
2016-06-27 16:22:37 +0200

    dynamic flats pages

--- config.rb
+++ config.rb
@@ -56,6 +56,15 @@ set :js_dir, 'javascripts'
 
 set :images_dir, 'images'
 
+
+# Dynamic pages construction
+
+["anne", "seb", "romain", "antoine"].each do |name|
+  proxy "/flats/#{name}.html", "/flats/show.html", :locals => { :owner => name }, :ignore => true
+end
+
+
+
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
